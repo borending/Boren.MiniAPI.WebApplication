1a37a1b4f6a79a867148e0cb1ca1d12922d0f5f0
Boren <boren@loansky.net>
2024-06-20 13:14:42 +0800

    clean code

--- Program.cs
+++ Program.cs
@@ -1,51 +1,60 @@
 using Boren.MiniAPI.WebApplication;
+using Boren.MiniAPI.WebApplication.Data;
+using Boren.MiniAPI.WebApplication.Data.Service;
 using Microsoft.EntityFrameworkCore;
 
-// builder 可以註冊DBContext，想當然也可以註冊Class
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
-var app = builder.Build();
-
-// 一、定義完整routing
-// API 設定domain/todoitems1 以HTTPGET 進行呼叫，回傳Todos 資料
-app.MapGet("/todoitems1", async (TodoDb db) =>
-    await db.Todos.ToListAsync());
-
-// 二、使用MapGroup，將routing 分類
-var todoItems = app.MapGroup("/todoitems2");
-// 等於domain/todoitems2/{id} 走HTTPGET 
-todoItems.MapGet("/{id}", async (int id, TodoDb db) =>
-    await db.Todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound());
-// 等於domain/ 走HTTPPOST
-todoItems.MapPost("/", async (Todo todo, TodoDb db) =>
+// 擴充類別、方法，將Endpoints 註冊集中
+public static class Endpoints
 {
-    db.Todos.Add(todo);
-    await db.SaveChangesAsync();
-    return Results.Created($"/{todo.Id}", todo);
-});
-
-// 三、從lambda 改成獨立方法
-static async Task<IResult> DeleteTodo(int id, TodoDb db)
-{
-    if (await db.Todos.FindAsync(id) is Todo todo)
+    public static void MapClassEndpoints(this WebApplication app)
     {
-        db.Todos.Remove(todo);
-        await db.SaveChangesAsync();
-        return TypedResults.NoContent();
+        var group = app.MapGroup("/class");
+        group.MapGet("/", () => "Get classes list");
     }
 
-    // Results 與TypedResults 差異
-    // Results 回傳IResult，TypedResults 回傳IResult 的實作
-    // 意即IResult 要做型別轉換才能知道實體類別
-    // 所以推薦使用TypedResults 以便於程式碼閱讀及測試
-    return TypedResults.NotFound();
+    public static void MapStudentEndpoints(this WebApplication app)
+    {
+        // 1. 可以定義完整routing
+        // API 設定 https://{domain}/students/{id} 以HTTPGET 進行呼叫，回傳資料
+        app.MapGet("/students/{Id}", (int id) =>
+        {
+            return Results.Ok(new Student { Name = "Amy" });
+            //return TypedResults.Ok(new Student { Name = "Amy" });
+
+            // Results 與TypedResults 差異
+            // Results 回傳IResult，TypedResults 回傳IResult 的實作
+            // 意即IResult 要做型別轉換才能知道實體類別
+            // 所以推薦使用TypedResults 以便於程式碼閱讀及測試
+        });
+
+        // return Results 需要呼叫Produces 以供OpenApi 產生文件
+        // return TypedResults 則不需要
+        // app.MapGet("/hello", () => Results.Ok(new Todo() { Name = "Hello World!" }))
+        //    .Produces<Todo>();
+        // app.MapGet("/hello2", () => TypedResults.Ok(new Todo() { Name = "Hello World!" }));
+
+        // 2. 也可以分群以便於管理
+        // 業務邏輯也可以採用依賴注入
+        var group = app.MapGroup("/students");
+        group.MapGet("/", (IStudentService service) => service.ReadAll());
+        group.MapPost("/", (Student student, IStudentService service) => service.Create(student));
+    }
 }
-todoItems.MapDelete("/{id}", DeleteTodo);
 
-// return Results 需要呼叫Produces 以供OpenApi 產生文件
-// return TypedResults 則不需要
-app.MapGet("/hello", () => Results.Ok(new Todo() { Name = "Hello World!" }))
-    .Produces<Todo>();
-app.MapGet("/hello2", () => TypedResults.Ok(new Todo() { Name = "Hello World!" }));
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        // builder 可以註冊DBContext，想當然也可以註冊類別
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+        builder.Services.AddScoped<IStudentService, StudentService>();
+        var app = builder.Build();
 
-app.Run();
+        // 利用擴充方法綁定Endpoints
+        app.MapClassEndpoints();
+        app.MapStudentEndpoints();
+
+        app.Run();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Boren.MiniAPI.WebApplication
+## ASP.NET Core Minimal API 基本架構
+將API routing, mapping, 程式邏輯 全寫在Program.cs 的Main 當中。也可以設定、產生OpenAPI 文件
+## 如何優化
+隨著提供的API 數量增加，Program.cs 就會變得越來越難維護。此時可以將功能模組化、歸類。並配合依賴注入，除了讓程式架構更乾淨以外，也能讓程式物件導向化。
+## 加入身分驗證
+待研究...
